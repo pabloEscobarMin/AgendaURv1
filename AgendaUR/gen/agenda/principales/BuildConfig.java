630a1a721cb62c95cbfeb4a74e8e9705012d1dea
germanSedano <gaiaelcaballero@gmail.com>
2014-04-03 18:36:20 +0200

    Cambios Sprint3

---
+++ AgendaUR/gen/agenda/principales/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package agenda.principales;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AgendaUR/src/agenda/principales/Eventos.java
@@ -0,0 +1,7 @@
+package agenda.principales;
+
+import android.app.Activity;
+
+public class Eventos extends Activity {
+
+}
